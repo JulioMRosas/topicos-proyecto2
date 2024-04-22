91f314b775b74c6372a262c8475ee526f9a5fc7f
Ibby <al21760482@ite.edu.mx>
2024-04-22 10:06:09 -0700

    se configuro firebase y se creo un componente para signin

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,9 +1,53 @@
-import { Text, View, Button } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useState } from "react";
 
 const SignIn = () => {
+  const [message, setMessage] = useState(false)
+
+  const signIn = () => {
+    setMessage(true);
+  }
+
   return (
-    <Text>SignIn</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Remembership</Text>
+      <TouchableOpacity style={styles.signInButton} onPress={signIn}>
+        <Text style={styles.signInButtonText}>Sign In</Text>
+      </TouchableOpacity>
+      { message && <Text>tas logeado</Text>}
+    </SafeAreaView>
   )
 }
 
 export default SignIn;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    color: "#8e44ad",
+    marginBottom: 20,
+  },
+
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  signInButton: {
+    width: "50%",
+    backgroundColor: "#8e44ad",
+    borderRadius: 12,
+    padding: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+
+  signInButtonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
---
+++ utils/firebase.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyA0Pr5kjwxBJibmmWnTSq3AfomeZncQbB0",
+
+  authDomain: "remembership-af366.firebaseapp.com",
+
+  projectId: "remembership-af366",
+
+  storageBucket: "remembership-af366.appspot.com",
+
+  messagingSenderId: "529353936405",
+
+  appId: "1:529353936405:web:9417a8c29001faa89c881c"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
