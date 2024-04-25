3ea2cfe89d7890008c2b5e0885deb7703b5368bd
SantanaLicon <spor@outlook.cl>
2024-04-24 19:40:10 -0700

    Diseño general estatico de la aplicación

--- App.js
+++ App.js
@@ -1,27 +1,43 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import SignIn from './components/SignIn';
+import { useState } from "react";
+import {
+  SafeAreaView,
+  SafeAreaViewComponent,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import SignIn from "./components/SignIn";
 
 export default function App() {
-const [user, setUser] = useState(false);
+  const [user, setUser] = useState(true);
 
   return (
     <View style={styles.container}>
-      {user ? (<Text style={styles.text}>Hola</Text>) : <SignIn/>}
+      {user ? <SafeAreaView></SafeAreaView> : <SignIn />}
+      <View style={styles.rectangulo}></View>
+      <View style={styles.rectangulo}></View>
+      <View style={styles.rectangulo}></View>
+      <View style={styles.footer}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, // Set the container to flex 1 to occupy the full screen
+    backgroundColor: "#fa7070",
   },
-
-  text: {
-    color: 'red'
+  rectangulo: {
+    width: "90%",
+    height: "20%",
+    margin: 20,
+    borderRadius: 25,
+    backgroundColor: "#fefded",
+  },
+  footer: {
+    flex: 1,
+    marginTop: 90,
+    backgroundColor: "#c6ebc5",
+    minHeight: 200,
   }
-});
+})
