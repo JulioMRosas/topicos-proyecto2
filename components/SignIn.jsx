f044eba194aa6308edf59b469d403fe6ccde2587
SantanaLicon <spor@outlook.cl>
2024-04-24 20:16:26 -0700

    Diseño general estatico de la aplicación 2

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,44 +1,72 @@
-import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useState } from "react";
 
 const SignIn = () => {
-  const [message, setMessage] = useState(false)
+  const [message, setMessage] = useState(false);
 
   const signIn = () => {
     setMessage(true);
-  }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Remembership</Text>
-      <TouchableOpacity style={styles.signInButton} onPress={signIn}>
-        <Text style={styles.signInButtonText}>Sign In</Text>
-      </TouchableOpacity>
-      { message && <Text>tas logeado</Text>}
+      <View style={styles.container2}>
+        <Text style={styles.welcome}>Bienvenido a:</Text>
+        <Text style={styles.remembership}>"Remembership"</Text>
+        <TouchableOpacity style={styles.signInButton} onPress={signIn}>
+          <Text style={styles.signInButtonText}>Iniciar sesión con Google</Text>
+        </TouchableOpacity>
+        {message && <Text>tas logeado</Text>}
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default SignIn;
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 30,
-    color: "#8e44ad",
+  remembership: {
+    fontSize: 23,
+    color: "#000000",
     marginBottom: 20,
+  },
+  container2: {
+    backgroundColor: "#fefded",
+    padding: 25,
+    borderRadius: 35,
+    width: "75%",
+    height: "40%",
+    alignItems: "center",
+    borderWidth: 5, // Add border width
+    borderColor: "#c6ebc5", // Set border color
+  },
+
+  welcome: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 5,
+    padding: 20,
+    fontFamily: "Quicksand-Regular",
   },
 
   container: {
     flex: 1,
-    width: '100%',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#E06666",
   },
 
   signInButton: {
     width: "50%",
-    backgroundColor: "#8e44ad",
+    backgroundColor: "#a1c398",
     borderRadius: 12,
     padding: 15,
     alignItems: "center",
@@ -47,7 +75,9 @@ const styles = StyleSheet.create({
   },
 
   signInButtonText: {
-    color: "#fff",
+    color: "#000000",
     fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
